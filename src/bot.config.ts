8807a8b854dc37b2885e495d3777ebc35dffc06e
Joaquim Neto <joaquimmy@gmail.com>
2021-06-20 17:43:09 -0300

    feat(command): add a few feedback messages to the config

--- src/bot.config.ts
+++ src/bot.config.ts
@@ -1,6 +1,7 @@
 import { IBotMessage } from './interfaces';
 import { Snowflake } from 'discord.js';
 import appRoot from 'app-root-path';
+import { TextHelpers } from './client/helpers';
 
 export interface ILevelPerm {
   level: number;
@@ -32,7 +33,13 @@ export interface IConfig {
   };
   permLevels: ILevelPerm[];
   messages: {
-    [key: string]: string;
+    COOLDOWN: string;
+    USAGE: string;
+    COMMAND_FEEDBACK_SERVER_ONLY: string;
+    COMMAND_FEEDBACK_DM_ONLY: string;
+    COMMAND_FEEDBACK_MISSING_PERMISSION: string;
+    COMMAND_FEEDBACK_MISSING_ARGS_SINGULAR: string;
+    COMMAND_FEEDBACK_MISSING_ARGS_PLURAL: string;
   };
   helpCategoryEmotes?: {
     [key: string]: string;
@@ -57,6 +64,23 @@ export const defaultConfig: IConfig = {
   messages: {
     COOLDOWN: 'Please wait **{0}** before using the {1} command again.',
     USAGE: "You're missing the **{0}** argument! \nUsage: {1}",
+    COMMAND_FEEDBACK_SERVER_ONLY:
+      'The {0} command is unavailable via private message. Please run it in a server.',
+    COMMAND_FEEDBACK_DM_ONLY:
+      'The {0} command is only available via private message. Please run it in the DMs.',
+    COMMAND_FEEDBACK_MISSING_PERMISSION: TextHelpers.lines(
+      'You do not have permission to use this command.',
+      `Your permission level is {0} ({1})`,
+      `This command requires level {2} ({3})`,
+    ),
+    COMMAND_FEEDBACK_MISSING_ARGS_SINGULAR: TextHelpers.lines(
+      `Looks like you have a problem with your args.`,
+      '{0}',
+    ),
+    COMMAND_FEEDBACK_MISSING_ARGS_PLURAL: TextHelpers.lines(
+      `Looks like you have a few problems with your args.`,
+      '{0}',
+    ),
   },
   permLevels: [
     // This is the lowest permisison level, this is for non-roled users.
